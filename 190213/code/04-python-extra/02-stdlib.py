bf6c3a30aaea57f16da245e5f4f6c5be7a33199c
Egor Suvorov <egor.suvorov@gmail.com>
2019-02-10 16:51:51 +0300

    190213/code/04-python-extra/02-stdlib: add lists multiplication

--- 190213/code/04-python-extra/02-stdlib.py
+++ 190213/code/04-python-extra/02-stdlib.py
@@ -10,3 +10,13 @@ c = collections.Counter()
 c.update([i % 7 for i in range(100)])
 print(c)
 print(c[0])
+
+# Multiplication
+print("Hi" * 2)
+print([1, 2] * 3)
+
+# Careful with references!
+a = [[0, 0]] * 4
+print(a)
+a[0][0] = 1
+print(a)
