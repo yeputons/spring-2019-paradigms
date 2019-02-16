c7638c4cdccc8863c8d8489ab893adfb403b75ab
Egor Suvorov <egor.suvorov@gmail.com>
2019-02-16 21:04:14 +0300

    190220/code/01-procedure/01-report-bad: make it worse

--- 190220/code/01-procedure/01-report-bad.py
+++ 190220/code/01-procedure/01-report-bad.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# ./01-report-bad.py --header Header --input 01-a.in --footer footer
 import sys
 from collections import defaultdict
 
@@ -8,20 +9,23 @@ max_acc_len = 7
 
 i = 1
 while i < len(sys.argv):
-    if sys.argv[i] == "--input":
+    if sys.argv[i][0] == "-" and sys.argv[i][2] == "i":  # --input
         i += 1
-        with open(sys.argv[i]) as f:
-            for line in f:
-                acc_from, acc_to, amount = line.split("\t")
-                amount = int(amount)
-                acc_balance[acc_from] -= amount
-                acc_balance[acc_to] += amount
-                max_acc_len = max(max_acc_len, len(acc_from))
-                max_acc_len = max(max_acc_len, len(acc_to))
-    elif sys.argv[i] == "--header":
+        f = open(sys.argv[i])
+        for line in f.read().split("\n"):
+            if len(line.split("\t")) > 2:
+                acc = line.split("\t")[0]
+                amount = line.split("\t")[2]
+                acc_balance[acc] -= int(amount)
+                max_acc_len = max(max_acc_len, len(acc))
+                acc = line.split("\t")[1]
+
+                acc_balance[acc] += int(amount)
+                max_acc_len = max(max_acc_len, len(acc))
+    elif sys.argv[i][0] == "-" and sys.argv[i][2] == "h":  # --header
         i += 1
         print("== " + sys.argv[i] + " ==")
-    elif sys.argv[i] == "--footer":
+    elif sys.argv[i][0] == "-" and sys.argv[i][2] == "f":  # --footer
         i += 1
         footer = sys.argv[i]
     else:
@@ -34,16 +38,16 @@ for balance in acc_balance.values():
     max_balance_len = max(max_balance_len, len(str(balance)))
 
 sys.stdout.write("|")
-for name in ["Account", "Balance"]:
+for i in ["Account", "Balance"]:
     sys.stdout.write(" " * (1 + max(0, (max_acc_len - 7) // 2)))
-    sys.stdout.write(name)
+    sys.stdout.write(i)
     sys.stdout.write(" " * (1 + max(0, (max_acc_len - 7) // 2)))
     sys.stdout.write("|")
 print()
 
-for account, balance in acc_balance.items():
+for account in acc_balance:
     line = "| " + account + " " * (max_acc_len - len(account)) + " | "
-    line += str(balance) + " " * (max_balance_len - len(str(balance))) + " |"
+    line += str(acc_balance[account]) + " " * (max_balance_len - len(str(acc_balance[account]))) + " |"
     print(line)
 
 
