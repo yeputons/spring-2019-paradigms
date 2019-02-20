eeac0003fb34186575f4875bd43eb10b7a531d45
Egor Suvorov <egor.suvorov@gmail.com>
2019-02-20 21:49:59 +0300

    190220/code: update 06-report-good.py based on the version from the class

--- 190220/code/01-procedure/06-report-good.py
+++ 190220/code/01-procedure/06-report-good.py
@@ -1,66 +1,74 @@
 #!/usr/bin/env python3
-# ./01-report-bad.py --header Header --input 01-a.in --footer footer
-from collections import namedtuple
 import sys
+import collections
 import argparse
+
+Transaction = collections.namedtuple(
+    'Transaction',
+    ['acc_from', 'acc_to', 'amount']
+)
+
+
+def read_transactions(filename):
+    result = []
+    with open(filename, 'r') as f:
+        for line in f.read().split('\n'):
+            if not line:
+                continue  # Early return/continue
+            acc_from, acc_to, amount = line.split('\t')
+            result.append(Transaction(
+                acc_from,
+                acc_to,
+                int(amount)
+            ))
+    return result
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', nargs='+')
 parser.add_argument('--header')
 parser.add_argument('--footer')
-args = parsers.parse_args()
+args = parser.parse_args()
 
-
-Transaction = collections.namedtuple('Transaction', ['acc_from', 'acc_to', 'amount'])
-
-
-def read_transactions(f):
-    result = []
-    with open(f) as f:
-        for line in f:
-            acc_from, acc_to, amount = line.split('\t')
-            result.append(Transaction(acc_from, acc_to, amount))
-    return result
-
+if args.header:
+    print('== ' + args.header + ' ==')
 
 transactions = []
-for fname in args['input']:
-    transactions += read_transactions(fname)
+for filename in args.input:
+    transactions += read_transactions(filename)
 
-
-def get_balaces(t):
-    acc_balance = collections.defaultdict(int)
+# TODO: extract into a separate procedure because transactions processing
+# may become a hard problem
+acc_balance = collections.defaultdict(int)
+for t in transactions:
     acc_balance[t.acc_from] -= t.amount
     acc_balance[t.acc_to] += t.amount
-    return acc_balance
 
+# TODO: use a sane library instead (Google it!)
+ACCOUNT_HEADER = 'Account'
+BALANCE_HEADER = 'Balance'
 
-acc_balances = get_balances(transactions)
+max_acc_len = max(len(ACCOUNT_HEADER), *[len(acc) for acc in acc_balance])
+# max_acc_len = max(7, *map(len, acc_balance))
+max_balance_len = max(len(BALANCE_HEADER), *[len(str(balance)) for balance in acc_balance.values()])
 
-if args['header']:
-    print('== ' + args['header'] + ' ==')
+def format_header(titles, width):
+    result = '|'
+    for title, width in zip(titles, width):
+        title_formatter = '{:^' + str(width + 2) + '}'
+        result += title_formatter.format(title)
+        result += '|'
+    return result
 
-# TODO
-max_balance_len = 7
-for balance in acc_balance.values():  # max + list comprehension
-    max_balance_len = max(max_balance_len, len(str(balance)))
-
-# Вывод таблички с центровкой и подсчёт данных в ячейках стоит разделить
-sys.stdout.write('|')
-for i in ['Account', 'Balance']:
-    # Не стоит сразу выводить на экран - так не протестировать.
-    # Например, тут есть баг и зависимость len('Account') == len('Balance') == 7.
-    sys.stdout.write(' ' * (1 + max(0, (max_acc_len - 7) // 2)))
-    sys.stdout.write(i)
-    sys.stdout.write(' ' * (1 + max(0, (max_acc_len - 7) // 2)))
-    sys.stdout.write('|')
-print()
+print(format_header([ACCOUNT_HEADER, BALANCE_HEADER],
+                    [max_acc_len, max_balance_len]))
 
 for account in acc_balance:
+    # TODO: refactor this like format_header()
     line = '| ' + account + ' ' * (max_acc_len - len(account)) + ' | '
     line += str(acc_balance[account]) + ' ' * (max_balance_len - len(str(acc_balance[account]))) + ' |'
     print(line)
 
 
-if args['footer']:
-    print(args['footer'])
+if args.footer:
+    print(args.footer)
