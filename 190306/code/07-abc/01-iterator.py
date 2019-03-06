167e7128e9bcd8b6ae0937f119061bc34a0767e2
Egor Suvorov <egor.suvorov@gmail.com>
2019-03-06 20:25:27 +0300

    190306/code: fix style for 07-abc

--- 190306/code/07-abc/01-iterator.py
+++ 190306/code/07-abc/01-iterator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import collections.abc
 
-class Naturals(collections.abc.Iterator):
+
+class Naturals(collections.abc.Iterator):  # Подсказка для PyCharm
     def __init__(self):
         self.value = 0
 
@@ -9,14 +10,17 @@ class Naturals(collections.abc.Iterator):
         self.value += 1
         return self.value
 
+
 it = Naturals()
-print(iter(it))  # WOW!
+print(iter(it))  # Автоматически получили от Iterator
 print(next(it))
 print(next(it))
 print(next(it))
 
-#def is_iter(o):
-#    return hasattr(o, '__next__') and на_самом_деле_соблюдает_контракт(o):
+
+# def is_iter(o):  # Проверка до abc
+#     return hasattr(o, '__next__') and на_самом_деле_соблюдает_контракт(o):
+
 
 def is_iter(o):
     return isinstance(o, collections.abc.Iterator)
--- 190306/code/07-abc/02-callable.py
+++ 190306/code/07-abc/02-callable.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import collections.abc
 
+
 class MyFunc(collections.abc.Callable):
     def __call__(self):
         return 0
+
 
 f = MyFunc()
 print(f())
