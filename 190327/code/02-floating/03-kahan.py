114c690f0f071249533a9af5a4120359a4381957
Egor Suvorov <egor.suvorov@gmail.com>
2019-03-26 15:14:26 +0300

    190327/code/03-kahan: add links to Wiki

--- 190327/code/02-floating/03-kahan.py
+++ 190327/code/02-floating/03-kahan.py
@@ -3,6 +3,8 @@ from random import shuffle
 def kahan_sum(xs):
     # Copied verbatim from https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
     # Is it good? I have no idea :(
+    # See https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%9A%D1%8D%D1%85%D1%8D%D0%BD%D0%B0
+    # See https://en.wikipedia.org/wiki/Kahan_summation_algorithm
     s = 0
     c = 0
     for x in xs:
